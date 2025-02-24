92abde9c26fd4c11bf3a0786a94902faa2203c49
Ivan Mikushin <imikushin@users.noreply.github.com>
2025-02-23 21:34:13 -0800

    WIP: wasm client lib

--- charms-client-wasm/src/lib.rs
+++ charms-client-wasm/src/lib.rs
@@ -1,4 +1,20 @@
 use wasm_bindgen::prelude::*;
 
-pub use charms_client as client;
-pub use charms_data as data;
+use charms_client;
+
+#[wasm_bindgen]
+pub fn extract_and_verify_spell(tx: JsValue, spell_vk: &str) -> anyhow::Result<JsValue, JsValue> {
+    let tx =
+        serde_wasm_bindgen::from_value(tx).map_err(|e| JsValue::from_str(&format!("{}", e)))?;
+    let spell = charms_client::tx::extract_and_verify_spell(&tx, spell_vk)
+        .map_err(|e| JsValue::from_str(&format!("{}", e)))?;
+    let spell =
+        serde_wasm_bindgen::to_value(&spell).map_err(|e| JsValue::from_str(&format!("{}", e)))?;
+    Ok(spell)
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn dummy() {}
+}

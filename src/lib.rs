6cf6edad8a9728d7516b581af406b2815fed0db6
Ivan Mikushin <imikushin@users.noreply.github.com>
2025-06-19 10:49:41 -0700

    update charms-spell-checker binary

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ pub mod utils;
 pub const SPELL_CHECKER_BINARY: &[u8] = include_bytes!("./bin/charms-spell-checker");
 
 /// Verification key for the `charms-spell-checker` binary.
-pub const SPELL_VK: &str = "0x003233a2577e8d4f6efbe4b0b5ab4a478d424a9b3a75b421019391921cbd691c";
+pub const SPELL_VK: &str = "0x00b237b3247f780898218f454010bc18403189814a46bf2a537fa9e1b6e80a7b";
 
 #[cfg(test)]
 mod test {
